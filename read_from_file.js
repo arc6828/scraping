913d92ca356ba0bc9c8fdab342c597f35025d019
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-10-08 18:21:44 +0700

    Python

--- read_from_file.js
+++ read_from_file.js
@@ -1,6 +1,19 @@
 var fs = require('fs');
-let filename = "Trade Map - List of supplying markets for a product imported by China.html";
-fs.readFile(filename, (err, data) => {
-    if (err) throw err;
-    console.log(data.toString('utf8'));
+
+//READ COUNTRIES
+let filename_countries = "json/countries.json";
+let content_countries = fs.readFileSync(filename_countries).toString('utf8');
+let countries = JSON.parse(content_countries);
+//console.log(content);
+countries.forEach((country,index)=>{
+    console.log(country.name, country.code);
+});
+
+//READ PRODUCTS
+let filename_products = "json/products.json";
+let content_products = fs.readFileSync(filename_products).toString('utf8');
+let products = JSON.parse(content_products);
+//console.log(content);
+products.forEach((product,index)=>{
+    console.log(product.name, product.code);
 });
