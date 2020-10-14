8eb7a1b84fb3e09fba74324f66ae54d7c3118b62
Rutsinsukmee <rutsinsukmee@gmail.com>
2020-10-14 17:32:35 +0700

    scraping cheerio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
+const cheerio = require('cheerio');
 
 nightmare
 //TRADE MAP
@@ -37,6 +38,8 @@ nightmare
     //CHOOSE COUNTRY
     //156 means china
     .select('#ctl00_NavigationControl_DropDownList_Country', '156')
+
+    .wait(10000)
     
     //CHOOSE PRODUCT
 
@@ -74,6 +77,106 @@ nightmare
     //     });*/
 
     // })
+    .evaluate(function(){
+
+    //here is where I want to return the html body
+    return document.body.innerHTML;
+
+    })
+    .then(function(body){
+    //loading html body to cheerio
+        // var $ = cheerio.load(body);
+        // const scrapedData = [];
+        // const tableHeaders = [];
+        // $("#ctl00_PageContent_MyGridView1 > tbody > tr").each((index, element) => {
+        // if (index === 0) {
+        //     const ths = $(element).find("th");
+        //     $(ths).each((i, element) => {
+        //     tableHeaders.push(
+        //         $(element)
+        //         .text()
+        //         .toLowerCase()
+        //     );
+        //     });
+        //     return true;
+        // }
+        // const tds = $(element).find("td");
+        // const tableRow = {};
+        // $(tds).each((i, element) => {
+        //     tableRow[tableHeaders[i]] = $(element).text();
+        // });
+        // scrapedData.push(tableRow);
+        // });
+        // console.log(scrapedData);
+
+        var $ = cheerio.load(body);
+        const objData = {
+            country: '',
+            detail: {
+                month: '',
+                unit: '',
+                value: '',
+            }
+        }
+        var dataArray = [];
+        let data_country = '';
+        let data_month = '';
+        let data_unit = '';
+        let data_value = '';
+
+        // get month
+        $("#ctl00_PageContent_MyGridView1 > tbody > tr:nth-child(1) > td").each((index, element) => {
+            
+            console.log("month ==> " , $(element).text());
+
+            data_month = $(element).text();
+            objData.detail.month = data_month;
+            dataArray.push({objData: objData});
+        })
+
+        
+        // get unit
+        $("#ctl00_PageContent_MyGridView1 > tbody > tr:nth-child(2) > th").each((index, element) => {
+
+            console.log("unit ==> " , $(element).text());
+
+            data_unit = $(element).text();
+            objData.detail.unit = data_unit;
+            dataArray.push({objData: objData});
+        })
+
+
+        // get country EX.1
+        $("#ctl00_PageContent_MyGridView1 > tbody > tr:nth-child(3) > td:nth-child(2)").each((index, element) => {
+            console.log("country ==> " , $(element).text());
+        })
+
+        // get country EX.2
+        $("#ctl00_PageContent_MyGridView1 > tbody > tr:nth-child(3)").each((index, element) => {
+
+            console.log("country [2] ==> " , $($(element).find("td")[1]).text());
+
+            data_country = $($(element).find("td")[1]).text();
+            objData.country = data_country;
+            dataArray.push({objData: objData});
+        })
+        
+        // get value
+        $("#ctl00_PageContent_MyGridView1 > tbody > tr:nth-child(3) > td").each((index, element) => {
+            if ( index === 1 || index === 0 ) {
+                return true;
+            }
+            
+            console.log("value ==> " , $(element).text());
+
+            data_value = $(element).text();
+            objData.detail.value = data_value;
+            dataArray.push({objData: objData});
+            
+        })
+        console.log(objData)
+        console.log(`dataArray ====> `, JSON.stringify(dataArray));
+    })
     .catch(error => {
         console.error('Search failed:', error)
     })
