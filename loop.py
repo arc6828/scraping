341bc292705ab693266ed1d7bcb1c33c6bf313e2
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-10-22 18:00:14 +0700

    country from and to

--- loop.py
+++ loop.py
@@ -2,6 +2,15 @@ import pandas as pd
 import os
 import json
 
+# import json
+
+with open('json/final-countries.json') as f:
+    countries = json.load(f)
+    
+# country = [c for c in countries if c["name"] == "Pakistan"]
+# code =  country[0]["iso_code"]  if len(country) > 0  else  "-"
+# code
+
 paths = os.listdir("html")
 # html_files
 
@@ -14,6 +23,8 @@ files = [ { "name" : path,  "size" : int (os.stat("html/"+path).st_size / 1024) 
 for f in files : 
 
     if f["size"] > 0 : 
+        if f["name"][0:1] == "I" :
+            continue
         if ".zip" in f["name"] :
             continue 
         #IF FILE EXIST, CONTINUE
@@ -48,9 +59,16 @@ for f in files :
                 if cols[1] == "World" :
                     continue
                 #print(head.replace("M","")+"-01", cols[1], col, "kg" )
+
+                c_name = cols[1]
+                country = [c for c in countries if c["name"] == c_name]
+                country_to_code =  country[0]["iso_code"]  if len(country) > 0  else  "-"
+                country_from_code = str(f["name"]).split("_")[3]
+
                 data_list.append({
                     "date" : head.replace("M","")+"-01" ,
-                    "country" : cols[1],
+                    "country_from" : country_from_code,
+                    "country_to" : country_to_code,
                     "value" : col,
                     "unit" : "kg",
                 })
